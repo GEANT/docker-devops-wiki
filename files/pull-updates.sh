241685f62bb45a695248d23c569061bedb440433
Massimiliano Adamo <maxadamo@gmail.com>
2024-01-09 00:50:48 +0100

    minor

--- files/pull-updates.sh
+++ files/pull-updates.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 PATH=/usr/sbin:/usr/bin:/sbin:/bin:/usr/local/sbin:/usr/local/bin
 
-echo "Changes detected. Pulling updates..."
+echo "Hook received. Pulling updates..."
 cd /code
 git pull --quiet --rebase origin main  # there seems to be a problem with TTY when using git pull
 mkdocs build --site-dir /usr/share/nginx/html
